a4c64db8723667e5eae21c65c2b1ced2c8bf53fd
Chintan Choksi <chintan@Chintans-MacBook-Pro.local>
2014-07-25 14:51:17 -0400

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'nodemanager',
+  packages = [''], 
+  version = '0.1',
+  description = "Seattle's nodemanager package",
+  author = 'Seattle',
+  url = 'https://github.com/BuildSeash/nodemanager', # use the URL to the github repo
+  classifiers = [],
+)
